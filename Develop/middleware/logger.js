af010896650d2762d866b8b32114a42a1e060b03
Brandy Quinlan <brandyquinlan.bootcamp@gmail.com>
2021-02-24 10:37:16 -0600

    commit logger file

---
+++ Develop/middleware/logger.js
@@ -0,0 +1,9 @@
+const moment = require('moment');
+
+// Middleware to console.log full url path and date/time
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
+    next();
+};
+
+module.exports = logger;
